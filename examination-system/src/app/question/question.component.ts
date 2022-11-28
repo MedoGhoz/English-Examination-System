3101accacb2b1d11e3f710d62bb7267f44c06476
MedoGhoz <ghozmedo@gmail.com>
2022-11-28 19:18:48 +0200

    edited home page and question page

--- examination-system/src/app/question/question.component.ts
+++ examination-system/src/app/question/question.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
-
+  public width:number = 0;
+  minutes:number = 0;
+  seconds:number = 0;
+  timeLeft:number = 600;
+  interval:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.startTimer()
   }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+        this.minutes = Math.floor((this.timeLeft / 60));
+        this.seconds = (this.timeLeft % 60);
+      } else {
+        this.width = 0;
+      }
+    },1000)
+  }
+
+
 
 }
