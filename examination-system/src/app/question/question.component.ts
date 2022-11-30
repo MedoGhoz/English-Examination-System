fae651ecdc4f8e1ada1f61a52f123034af76d250
Ahmed Fakhreldin <84821705+ahmedfakhr1@users.noreply.github.com>
2022-11-30 16:45:12 +0200

    Comments Added
    Comments added

--- examination-system/src/app/question/question.component.ts
+++ examination-system/src/app/question/question.component.ts
@@ -8,15 +8,17 @@ import { QuestionService } from '../services/question.service';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
-  public width:number = 0;
+  public width:number = 0; //Number of Questions
+  /*Timer Variables*/
   minutes:number = 10;
   seconds:number = 0;
   timeLeft:number = 600;
   interval:any;
+  /*Questions related Variables */
   questions:any;
   question:any;
-  display:string = "block";
-  logoPath!:string;
+  display:string = "block"; //Display of the left Block of the Question
+  logoPath!:string; //Question Type
   type!:string;
   answers:any[] =[];
   constructor(private questionService:QuestionService, private router:Router) {
@@ -29,7 +31,7 @@ export class QuestionComponent implements OnInit {
     this.showQuestion();
     this.changelogo();
   }
-
+/*Handles the Timer of the Exam*/
   startTimer() {
     this.interval = setInterval(() => {
       if(this.timeLeft > 0) {
@@ -42,7 +44,10 @@ export class QuestionComponent implements OnInit {
       }
     },1000)
   }
-
+/*
+Decides what to view next
+pushes the answers of the student to the answer array in the service
+*/
    getSelection() { 
     let nextButton = document.querySelector(".nextButton");
     let checkRadio = document.querySelector('input[name="selection"]:checked');
@@ -56,6 +61,7 @@ export class QuestionComponent implements OnInit {
       this.questionService.setAnswers(this.answers);
       this.router.navigate(['/result']);
     }
+    //decide what to show in all Question
     if(this.width < 9){
       if(checkRadio == this.questions[this.width].answer) {
         
@@ -67,11 +73,12 @@ export class QuestionComponent implements OnInit {
       this.showQuestion();
       this.changelogo();
     }
+    //Last Question
     if(this.width == 9){
       nextButton?.setAttribute("value","finish");
     }
   }
-  
+  //decides based on the question type to show the left section of the question or not
   showQuestion(){
     if(this.questions[this.width].category === "Grammar" || this.questions[0].category === "vocabulary"){
       this.display = "none";
@@ -79,7 +86,7 @@ export class QuestionComponent implements OnInit {
       this.display = "block";
     }
   }
-
+//changes logo
   changelogo(){
     this.type = this.questions[this.width].category;
     if(this.type.toLocaleLowerCase() == "grammar"){
