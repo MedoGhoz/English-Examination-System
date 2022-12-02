c9c3828b560ae2d0625244d03936a3c287f29bbc
Ahmed Fakhreldin <84821705+ahmedfakhr1@users.noreply.github.com>
2022-12-02 20:45:16 +0200

    Changes in result component
    Fixing the problem or viewing the Youtube video after reload only,now it views instantly when showing the result component.

--- examination-system/src/app/results/results.component.ts
+++ examination-system/src/app/results/results.component.ts
@@ -15,22 +15,14 @@ export class ResultsComponent implements OnInit {
   iframeMarkup!: string;
 
   constructor(private questionService: QuestionService) { }
-
-  ngOnInit(): void {
-    this.apiQuestions = this.questionService.getQuestions();
+  ngAfterViewInit():void{
     this.formatQuestions();
-    this.answers = this.questionService.getAnswers();
-    for (let i = 0; i < this.answers.length; i++) {
-      if (this.answers[i] == this.questions[i].answer) {
-        this.result++;
-      }
-    }
   }
+  ngOnInit(): void {
 
-
-
-  formatQuestions() {
-    for (let i = 0; i < this.apiQuestions.length; i++) {
+    this.apiQuestions = this.questionService.getQuestions();
+//
+for (let i = 0; i < this.apiQuestions.length; i++) {
       if ("questions" in this.apiQuestions[i]) {
         for (let j = 0; j < this.apiQuestions[i].questions.length; j++) {
           let obj = JSON.parse(JSON.stringify(this.apiQuestions[i]));
@@ -64,6 +56,49 @@ export class ResultsComponent implements OnInit {
       } else {
         this.questions.push(this.apiQuestions[i]);
       }
+    }
+//
+    this.answers = this.questionService.getAnswers();
+    for (let i = 0; i < this.answers.length; i++) {
+      if (this.answers[i] == this.questions[i].answer) {
+        this.result++;
+      }
+    }
+
+  }
+
+
+
+  formatQuestions() {
+    for (let i = 0; i < this.apiQuestions.length; i++) {
+      if ("questions" in this.apiQuestions[i]) {
+        for (let j = 0; j < this.apiQuestions[i].questions.length; j++) {
+          let obj = JSON.parse(JSON.stringify(this.apiQuestions[i]));
+          if (j == 0) {
+            obj.first = true;
+          } else {
+            obj.first = false;
+          }
+          obj.question = this.apiQuestions[i].questions[j].question;
+          obj.choices = this.apiQuestions[i].questions[j].choices;
+          obj.answer = this.apiQuestions[i].questions[j].answer;
+          obj.type = this.apiQuestions[i].questions[j].type;
+            // DOM ready! Images, frames, and other subresources are still downloading.
+            if (obj.category.toLocaleLowerCase() == 'listening' && document.getElementById(String(j)) != null) {
+              /*convert the youtube link to iframe*/
+              const videoId = this.getId(obj.header);
+              this.iframeMarkup = '<iframe width="100%" height="250" src="//www.youtube.com/embed/'
+                + videoId + '" frameborder="0" allowfullscreen></iframe>';
+              console.log(String(j));
+
+              let node = document.getElementById(String(j));
+              console.log(node);
+
+              node!.innerHTML = this.iframeMarkup;
+            }
+
+        }
+      } 
     }
   }
 
