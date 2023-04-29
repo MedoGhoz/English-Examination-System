4f8f38984690f592acfa7bc9c996f4114030470e
Abdelrhman-Abdelaziz <85349614+Abdelrhman-Abdelaziz@users.noreply.github.com>
2023-04-29 17:11:00 +0300

    add question generation

---
+++ examination-system/src/app/_models/questionGenerationRequest.ts
@@ -0,0 +1,5 @@
+export class questionGenerationRequest {
+    context: string = "";
+    n_ques: number = 0;
+    n_mcq: number = 0;
+  } 
---
+++ examination-system/src/app/services/question-generation.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { questionGenerationRequest } from '../_models/questionGenerationRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionGenerationService {
+
+  private questionGenerationUrl = 'http://164.92.176.13/mcq';  // URL to web api
+  // private questionGenerationUrl = 'api/heroes';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor( private http: HttpClient ) { }
+
+  /** POST: add a new hero to the server */
+  generate(req: questionGenerationRequest): Observable<any> {
+    return this.http.post<any>(this.questionGenerationUrl, req, this.httpOptions).pipe(
+      // catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+}
